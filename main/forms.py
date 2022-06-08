4f780234b297510cef4093b29bbea1caf8be890f
nrgdcbg <reyes.allen13@gmail.com>
2022-06-08 14:39:30 +0800

    models, forms, other backend stuff added

---
+++ main/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+from django.forms import ModelChoiceField, ModelForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.models import User
+from .models import *
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = AuthUser
+        fields = ['first_name','last_name','username', 'email', 'password1', 'password2', 'user_type', 'address', 'birthdate', 'contact_no']
+
+class EditUserForm(UserChangeForm):
+    class Meta:
+        model = AuthUser
+        fields = ['first_name','last_name','username', 'email', 'password1', 'password2', 'user_type', 'address', 'birthdate', 'contact_no']
+
+class EditPhysicianForm(ModelForm):
+    class Meta:
+        model = Physician
+        fields = ['specialization', 'hospital_affiliation']
+
+class DocumentForm(ModelForm):
+    class Meta:
+        model = Document
+        fields = ['file']
+
+class PrescriptionForm(ModelForm):
+    class Meta:
+        model = Prescription
+        fields = ['file']
+
+class PatientConsultationRecordForm(ModelForm):
+    class Meta:
+        model = PatientConsultationRecord
+        fields = ['date']
--- telemed/settings.py
+++ telemed/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
+
+AUTH_USER_MODEL = 'main.AuthUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -118,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
