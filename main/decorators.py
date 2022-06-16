8d5fe4dabbbc09532c9b97d7a128ce577a1a59bb
JM-Franco <jefranco1@up.edu.ph>
2022-06-16 20:34:06 +0800

    Add auto-creation of PA/PH models on approve/deny

--- main/decorators.py
+++ main/decorators.py
@@ -6,8 +6,10 @@ def unauthenticated_user(view_func):
         if request.user.is_authenticated:
             if request.user.role == "SA":
                 return redirect("/account_requests/")
-            # elif request.user.role == "PH":
-            #     return redirect("login")
+            elif request.user.role == "PH":
+                return redirect("/account_requests/")
+            elif request.user.role == "PA":
+                return redirect("/account_requests/")
             else:
                 return view_func(request, *args, **kwargs)
         else:
--- main/signals.py
+++ main/signals.py
@@ -1,2 +1,11 @@
-from django.core.mail import send_mail
 from .models import *
+
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        if instance.role == "PH":
+            Physician.objects.create(account=instance)
+        elif instance.role == "PA":
+            Patient.objects.create(account=instance)
+        else:
+            pass
